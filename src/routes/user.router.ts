98ae653f43de475485a226a2332af5b00e356e53
wendelbueres <wendelbueres@gmail.com>
2023-02-20 02:00:21 -0300

    feat: :sparkles: rota createUser - POST

---
+++ src/routes/user.router.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import createUserController from "../controllers/user/user.create.controller";
+
+const userRouters = Router();
+
+userRouters.get("/:id");
+userRouters.post("", createUserController);
+userRouters.patch("");
+userRouters.delete("");
