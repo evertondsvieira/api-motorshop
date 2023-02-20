4711bd6c44f7bb13abb06aa62d2151d5e0526c40
wendelbueres <wendelbueres@gmail.com>
2023-02-20 12:35:10 -0300

    feat: :sparkles: importadas rotas

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
-import express from 'express'
+import "reflect-metadata";
+import "express-async-errors";
+import express from "express";
+import { handleErrorMidleware } from "./middlewares/handleError.midleware";
+import userRouters from "./routes/user.router";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+app.use("/users", userRouters);
+app.use(handleErrorMidleware);
 
-
-export default app
+export default app;
