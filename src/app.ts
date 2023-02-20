22213ae57433cd7aba04fefcfb709da1abd689da
wendelbueres <wendelbueres@gmail.com>
2023-02-20 13:57:58 -0300

    refactor: :hammer: importada rota user

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,11 @@ import "reflect-metadata";
 import "express-async-errors";
 import express from "express";
 import { handleErrorMidleware } from "./middlewares/handleError.midleware";
-import userRouters from "./routes/user.router";
+import userRoutes from "./routes/user.routes";
 
 const app = express();
 app.use(express.json());
-app.use("/users", userRouters);
+app.use("/users", userRoutes);
 app.use(handleErrorMidleware);
 
-export default app
+export default app;
