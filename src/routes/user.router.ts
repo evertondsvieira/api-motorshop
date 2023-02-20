6ee9addb53eaba3670647db5b25cb5bc58ed6fd2
wendelbueres <wendelbueres@gmail.com>
2023-02-20 13:01:44 -0300

    feat: :sparkles: rota listDetailsUser

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import createUserController from "../controllers/user/user.create.controller";
+import listDetailsUserController from "../controllers/user/user.listDetails.controller";
 
 const userRouters = Router();
 
-userRouters.get("/:id");
+userRouters.get("/:id", listDetailsUserController);
 userRouters.post("", createUserController);
 userRouters.patch("");
 userRouters.delete("");
