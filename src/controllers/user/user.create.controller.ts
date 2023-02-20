8df1eaa759b9cb111c8fbf747ed8a8e093203381
wendelbueres <wendelbueres@gmail.com>
2023-02-20 01:53:39 -0300

    feat: :sparkles: controller createUser

---
+++ src/controllers/user/user.create.controller.ts
@@ -0,0 +1,12 @@
+import { instanceToPlain } from "class-transformer";
+import { Request, Response } from "express";
+import createUserService from "../../services/user/createUser.services";
+
+export default async function createUserController(
+  req: Request,
+  res: Response
+) {
+  const user = req.body;
+  const createdUser = createUserService(user);
+  return res.status(201).json(instanceToPlain(createdUser));
+}
