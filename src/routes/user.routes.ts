d8fe51876accd964185759728883f48d5b3de394
wendelbueres <wendelbueres@gmail.com>
2023-02-20 13:58:33 -0300

    refactor: :hammer: renomeada rota

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,13 @@
-import { Router } from "express"
-import createUserController from "../controllers/user/user.create.controller"
+import { Router } from "express";
+import createUserController from "../controllers/user/user.create.controller";
+import deleteUserController from "../controllers/user/user.delete.controller";
+import listDetailsUserController from "../controllers/user/user.listDetails.controller";
 
-const userRoutes = Router()
+const userRoutes = Router();
 
-userRoutes.post("", createUserController)
+userRoutes.get("/:id", listDetailsUserController);
+userRoutes.post("", createUserController);
+userRoutes.patch("");
+userRoutes.delete("/:id", deleteUserController);
 
-
-export default userRoutes
+export default userRoutes;
