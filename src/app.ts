7f8dded29b5f444a360dd200f4e2fea29bd5d380
rafael <rafael.a.f.1984@gmail.com>
2023-02-22 10:59:50 -0300

    feat: implementação da rota

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,14 @@ import express from "express";
 import { handleErrorMidleware } from "./middlewares/handleError.midleware";
 import userRoutes from "./routes/user.routes";
 import loginRoutes from "./routes/session.routes";
+import annoucementeRoutes from "./routes/annoucements.routes";
+
 
 const app = express();
 app.use(express.json());
 app.use("/users", userRoutes);
-app.use("/login", loginRoutes)
+app.use("/login", loginRoutes);
+app.use("/annoucements", annoucementeRoutes);
 app.use(handleErrorMidleware);
 
 export default app;
