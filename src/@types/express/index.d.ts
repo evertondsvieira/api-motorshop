605935899f025b333f0624eb96998a073f645338
wendelbueres <wendelbueres@gmail.com>
2023-02-22 10:36:47 -0300

    fix: :bug: typagem global requisição

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,12 +1,14 @@
 import * as express from "express";
 
 declare global {
-    namespace Express {
-        interface Request {
-            validatedBody: object;
-            user: {
-                id: string;
-            };
-        }
+  namespace Express {
+    interface Request {
+      validatedBody: object;
+      user: {
+        id: string;
+        isActive: boolean;
+        isAdvertiser: boolean;
+      };
     }
+  }
 }
