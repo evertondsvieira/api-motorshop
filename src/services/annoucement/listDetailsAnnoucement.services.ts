ba48168c4210fcde1685b0cf92d2dd75bff980fb
wendelbueres <wendelbueres@gmail.com>
2023-02-23 15:28:34 -0300

    refactor: :hammer: renomeados services

---
+++ src/services/annoucement/listDetailsAnnoucement.services.ts
@@ -0,0 +1,24 @@
+import { AppDataSource } from "../../data-source";
+import { EntityNotFoundError } from "typeorm";
+import { AppError } from "../../errors";
+import { Annoucements } from "../../entities/annoucements.entity";
+
+const listDetailsAnnoucementsService = async (id: string) => {
+  try {
+    const annoucemntRequesty = AppDataSource.getRepository(Annoucements);
+
+    const annoucementsFind = await annoucemntRequesty.findOneBy({
+      annoucementId: id,
+    });
+
+    return annoucementsFind;
+  } catch (err) {
+    if (err instanceof EntityNotFoundError) {
+      throw new AppError("Annoucement not found", 404);
+    } else {
+      throw new AppError("Internal error", 500);
+    }
+  }
+};
+
+export default listDetailsAnnoucementsService;
