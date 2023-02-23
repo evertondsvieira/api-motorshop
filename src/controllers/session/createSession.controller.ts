076a4b6654a2e872e7c484533d5380cf0ebc65cd
wendelbueres <wendelbueres@gmail.com>
2023-02-23 10:46:53 -0300

    fix: :bug: importação

--- src/controllers/session/createSession.controller.ts
+++ src/controllers/session/createSession.controller.ts
@@ -1,13 +1,13 @@
 import { IUserLogin } from "../../interfaces/user";
 import { Request, Response } from "express";
-import createSessionService from "../../services/session/createsSession.service";
+import createSessionService from "../../services/session/createSession.services";
 
 const createSessionController = async (req: Request, res: Response) => {
-    const session: IUserLogin = req.body;
+  const session: IUserLogin = req.body;
 
-    const token = await createSessionService(session);
+  const token = await createSessionService(session);
 
-    return res.status(200).json(token);
+  return res.status(200).json(token);
 };
 
 export { createSessionController };
