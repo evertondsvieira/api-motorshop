bdc82f9f550a5bf754eb81443e9f8adba25d40d8
Everton Vieira <vieiraeverton33@hotmail.com>
2023-02-23 15:23:49 -0300

    Feat: Create annoucement route

--- src/routes/annoucement.routes.ts
+++ src/routes/annoucement.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import createAnnoucementController from "../controllers/annoucement/createAnnoucement.controller";
+import deleteAnnouncementsController from "../controllers/annoucement/deleteAnnouncement.controller";
 
 const annoucementRoutes = Router();
 
 annoucementRoutes.post("", createAnnoucementController);
+annoucementRoutes.delete("/:id", deleteAnnouncementsController);
 
 export default annoucementRoutes;
