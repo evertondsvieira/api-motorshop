7de7c98783654adc404dd5276e114aa9366ba4f4
wendelbueres <wendelbueres@gmail.com>
2023-02-23 21:40:02 -0300

    fix: :bug: router announcement

--- src/routes/annoucements.routes.ts
+++ src/routes/annoucements.routes.ts
@@ -1,19 +1,15 @@
 import { Router } from "express";
 import createAnnoucementController from "../controllers/annoucement/createAnnoucement.controller";
 import listAnnoucementsController from "../controllers/annoucement/listAnnoucements.controller";
-import listDatailsAnnoucementController from "../controllers/annoucement/listDetailsAnnoucement.controller";
+import listDetailsAnnoucementController from "../controllers/annoucement/listDetailsAnnoucement.controller";
 import updateAnnouncementController from "../controllers/annoucement/updateAnnoucement.controller";
 import authUserMiddleware from "../middlewares/auth.user.middleware";
 
 const announcementsRoutes = Router();
 
-announcementsRoutes.post(
-  "/:userId",
-  authUserMiddleware,
-  createAnnoucementController
-);
-announcementsRoutes.get("/:userId", listAnnoucementsController);
-announcementsRoutes.get("/:annoucementId", listDatailsAnnoucementController);
+announcementsRoutes.post("", authUserMiddleware, createAnnoucementController);
+announcementsRoutes.get("", listAnnoucementsController);
+announcementsRoutes.get("/:annoucementId", listDetailsAnnoucementController);
 announcementsRoutes.patch(
   "/:annoucementId",
   authUserMiddleware,
