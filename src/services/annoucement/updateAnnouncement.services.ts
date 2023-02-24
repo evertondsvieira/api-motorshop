7dd945423367a5224b4d3fb04d250f832101d546
wendelbueres <wendelbueres@gmail.com>
2023-02-23 22:28:27 -0300

    feat: :sparkles: adicionadas verificações

--- src/services/annoucement/updateAnnouncement.services.ts
+++ src/services/annoucement/updateAnnouncement.services.ts
@@ -1,6 +1,7 @@
 import { AppDataSource } from "../../data-source";
 import { Annoucements } from "../../entities/annoucements.entity";
 import { User } from "../../entities/user.entity";
+import { AppError } from "../../errors";
 import { IAnnoucement } from "../../interfaces/annoucement";
 
 const updateAnnoucementService = async (
@@ -10,9 +11,12 @@ const updateAnnoucementService = async (
 ) => {
   const annoucementRepository = AppDataSource.getRepository(Annoucements);
   const userRepository = AppDataSource.getRepository(User);
-  const user = await userRepository.findOneOrFail({ where: { id: idUser } });
+  const user = await userRepository.findOneOrFail({
+    where: { id: idUser },
+  });
   let annoucement = await annoucementRepository.findOneOrFail({
     where: { annoucementId: id },
+    relations: { user: true },
   });
 
   if (Object.keys(data).length > 0) {
@@ -28,6 +32,21 @@ const updateAnnoucementService = async (
       "isActive",
     ];
 
+    const typeVehicle = ["motocycle", "car"];
+    const adType = ["sale", "auction"];
+
+    if (data.vehicleType) {
+      if (!typeVehicle.includes(data.vehicleType)) {
+        throw new AppError("value in field vehicleType is not valid");
+      }
+    }
+
+    if (data.adType) {
+      if (!adType.includes(data.adType)) {
+        throw new AppError("value in field adType is not valid");
+      }
+    }
+
     const keys = Object.keys(data);
     const include = keys.map((key) => {
       if (fields.includes(key)) {
@@ -37,7 +56,7 @@ const updateAnnoucementService = async (
     });
 
     if (include.includes(true)) {
-      if (user.id === annoucement.user.id) {
+      if (annoucement.user.id === user.id) {
         await annoucementRepository.update(id, data);
         annoucement = await annoucementRepository.findOneOrFail({
           where: { annoucementId: id },
@@ -45,8 +64,18 @@ const updateAnnoucementService = async (
       }
     }
 
+    annoucement = await annoucementRepository.findOneOrFail({
+      where: { annoucementId: id },
+      relations: { user: false },
+    });
+
     return annoucement;
   }
+
+  annoucement = await annoucementRepository.findOneOrFail({
+    where: { annoucementId: id },
+    relations: { user: false },
+  });
 
   return annoucement;
 };
