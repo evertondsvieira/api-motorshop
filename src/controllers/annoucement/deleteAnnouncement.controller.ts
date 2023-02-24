09dc65ea3ce117cf3b60fe012170f820104e579b
wendelbueres <wendelbueres@gmail.com>
2023-02-23 23:49:13 -0300

    fix: :bug: id req.params

--- src/controllers/annoucement/deleteAnnouncement.controller.ts
+++ src/controllers/annoucement/deleteAnnouncement.controller.ts
@@ -2,8 +2,8 @@ import { Request, Response } from "express";
 import deleteAnnouncementsService from "../../services/annoucement/deleteAnnoucement.services";
 
 const deleteAnnouncementsController = async (req: Request, res: Response) => {
-  const id: string = req.params.id;
-  const { id: userId } = req.user;
+  const id: string = req.params.annoucementId;
+  const userId = req.user.id;
   await deleteAnnouncementsService(id, userId);
   return res.status(204).json({ message: "Successfully Deleted" });
 };
