51ae2234fee7280a589ffa0bb76533cff6e1f1b4
wendelbueres <wendelbueres@gmail.com>
2023-02-23 21:40:41 -0300

    feat: :sparkles: adicionado authUaer em routers users

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import createUserController from "../controllers/user/user.create.controller";
 import deleteUserController from "../controllers/user/user.delete.controller";
 import listDetailsUserController from "../controllers/user/user.listDetails.controller";
+import authUserMiddleware from "../middlewares/auth.user.middleware";
 
 const userRoutes = Router();
 
 userRoutes.get("/:id", listDetailsUserController);
 userRoutes.post("", createUserController);
 userRoutes.patch("");
-userRoutes.delete("/:id", deleteUserController);
+userRoutes.delete("/:id", authUserMiddleware, deleteUserController);
 
 export default userRoutes;
