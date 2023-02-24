cd86515938d2866fb1de8a91d97d2e7cd455af97
wendelbueres <wendelbueres@gmail.com>
2023-02-23 21:39:14 -0300

    fix: :bug: announcement interface

--- src/interfaces/annoucement/index.ts
+++ src/interfaces/annoucement/index.ts
@@ -1,19 +1,16 @@
-import { IUser } from "../user";
-
-
 export interface IAnnoucementRequest {
-    title: string;
-    isActive: boolean;
-    adType: string;
-    year: number;
-    mileage: number;
-    price: number;
-    description: string;
-    vehicleType: string;
-    coverImage: string;
+  title: string;
+  isActive: boolean;
+  adType: string;
+  year: number;
+  mileage: number;
+  price: number;
+  description: string;
+  vehicleType: string;
+  coverImage: string;
 }
 
-export interface IAnnoucement extends IUser {
-    id: number;
-    userId: number;
+export interface IAnnoucement extends IAnnoucementRequest {
+  id: string;
+  userId: number;
 }
