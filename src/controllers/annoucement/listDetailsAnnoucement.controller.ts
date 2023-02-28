3ba67f1a9f7744c25dbe8efe1d307ac427b8b648
wendelbueres <wendelbueres@gmail.com>
2023-02-28 07:41:40 -0300

    fix: :bug: listDetailsAnnouncement

--- src/controllers/annoucement/listDetailsAnnoucement.controller.ts
+++ src/controllers/annoucement/listDetailsAnnoucement.controller.ts
@@ -5,8 +5,8 @@ const listDetailsAnnoucementController = async (
   req: Request,
   res: Response
 ) => {
-  const { id } = req.user;
-  const annoucement = await listDetailsAnnoucementsService(id);
+  const { annoucementId } = req.params;
+  const annoucement = await listDetailsAnnoucementsService(annoucementId);
 
   return res.status(200).json(annoucement);
 };
