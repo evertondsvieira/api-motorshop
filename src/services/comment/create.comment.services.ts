9bfd0ebea79df7975307f5d117b5d88b90aeeb9d
wendelbueres <wendelbueres@gmail.com>
2023-02-28 07:56:14 -0300

    refactor: :hammer: service createComment

--- src/services/comment/create.comment.services.ts
+++ src/services/comment/create.comment.services.ts
@@ -3,16 +3,31 @@ import { AppError } from "../../errors";
 import { User } from "../../entities/user.entity";
 import { IComment } from "../../interfaces/comment";
 import { Comments } from "../../entities/comments.entity";
+import { Annoucements } from "../../entities/annoucements.entity";
 
-async function createCommentService(data: IComment, id: string) {
+async function createCommentService(
+  data: IComment,
+  id: string,
+  idAnnouncement: string
+) {
   const commentRepository = AppDataSource.getRepository(Comments);
   const userRepository = AppDataSource.getRepository(User);
+  const announcementRepository = AppDataSource.getRepository(Annoucements);
   const user = await userRepository.findOne({
     where: { id: id },
+  });
+  const announcement = await announcementRepository.findOne({
+    where: {
+      annoucementId: idAnnouncement,
+    },
   });
 
   if (!user) {
     throw new AppError("User not found", 404);
+  }
+
+  if (!announcement) {
+    throw new AppError("Announcement not found", 404);
   }
 
   if (!data.text) {
@@ -22,6 +37,7 @@ async function createCommentService(data: IComment, id: string) {
   const comment = commentRepository.create(data);
 
   comment.user = user;
+  comment.annoucements = announcement;
 
   await commentRepository.save(comment);
 
