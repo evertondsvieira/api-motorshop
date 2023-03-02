3fb1becbcc7399d9fafac79cde3466c6f7cddfe7
Everton Vieira <vieiraeverton33@hotmail.com>
2023-03-02 10:00:30 -0300

    fix: alterações nas migrações

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,11 +4,8 @@ import { User } from "./entities/user.entity";
 import { Address } from "./entities/address.entity";
 import { Comments } from "./entities/comments.entity";
 import { Annoucements } from "./entities/annoucements.entity";
-import { initial1677204054785 } from "./migrations/1677204054785-initial";
-import { relationsComments1677504427789 } from "./migrations/1677504427789-relations-comments";
-import { fieldComments1677541765010 } from "./migrations/1677541765010-field-comments";
-import { changerFieldIdComment1677581525321 } from "./migrations/1677581525321-changer-field-id-comment";
-import { changerMaxPriceAnnouncement1677626229781 } from "./migrations/1677626229781-changer-max-price-announcement";
+import { initialMigration1677677803015 } from "./migrations/1677677803015-initialMigration";
+
 
 const dataSourceConfig = (): DataSourceOptions => ({
   type: "postgres",
@@ -18,15 +15,11 @@ const dataSourceConfig = (): DataSourceOptions => ({
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
   logging: true,
-  synchronize: false,
+  synchronize: true,
 
   entities: [User, Address, Comments, Annoucements],
   migrations: [
-    initial1677204054785,
-    relationsComments1677504427789,
-    fieldComments1677541765010,
-    changerFieldIdComment1677581525321,
-    changerMaxPriceAnnouncement1677626229781,
+    initialMigration1677677803015
   ],
 });
 
