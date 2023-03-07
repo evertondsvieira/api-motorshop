84d7129b549cd130026fb8be48d13e950518de4f
wendelbueres <wendelbueres@gmail.com>
2023-03-07 20:08:14 -0300

    refactor: :hammer: rota address movida para rotas de users

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,6 @@ import { handleErrorMidleware } from "./middlewares/handleError.midleware";
 import userRoutes from "./routes/user.routes";
 import loginRoutes from "./routes/session.routes";
 import announcementsRoutes from "./routes/annoucements.routes";
-import addressRoutes from "./routes/address.routes";
-import authUserMiddleware from "./middlewares/auth.user.middleware";
 import routerResetPassword from "./routes/forgot.routes";
 import commentsRoutes from "./routes/comment.routes";
 const app = express();
@@ -18,7 +16,6 @@ app.use((req, res, next) => {
   app.use(cors());
   app.use(express.json());
   app.use("/users", userRoutes);
-  app.use("/users/address", authUserMiddleware, addressRoutes);
   app.use("/login", loginRoutes);
   app.use("/announcements", announcementsRoutes);
   app.use("/comments", commentsRoutes);
--- src/routes/address.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import addressUpdateController from "../controllers/address/address.update.controller";
-
-const addressRoutes = Router();
-
-addressRoutes.patch("", addressUpdateController);
-
-export default addressRoutes;
